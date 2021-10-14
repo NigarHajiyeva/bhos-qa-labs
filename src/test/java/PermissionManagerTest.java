39e2e8960484628c3103a05455ecc89526c33c8a
NigarHajiyeva <64946039+NigarHajiyeva@users.noreply.github.com>
2021-10-14 15:03:31 +0400

    Update PermissionManagerTest.java

--- src/test/java/PermissionManagerTest.java
+++ src/test/java/PermissionManagerTest.java
@@ -1,5 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-class PermissionManagerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class PermissionManagerTest {
+    public PermissionManager permissionManager = new PermissionManager();
+
+    @Test
+    @DisplayName("Testing default value")
+    void testSolution() {
+        assertEquals("User", permissionManager.getmCurrentLevel());
+    }
+
+    @Test
+    @DisplayName("Testing setter method ")
+    void testSetter() {
+        permissionManager.setmCurrentLevel("Admin");
+        assertEquals("Admin", permissionManager.getmCurrentLevel());
+    }
 }
