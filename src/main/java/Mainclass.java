3860bd71765225db62bf763749697cfd10010d3f
NigarHajiyeva <nhaciyeva183@gmail.com>
2021-10-14 21:12:19 +0400

    Adding Lab 4

---
+++ src/main/java/Mainclass.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mainclass {
+    public static void main(String[] args) {
+        Student tom = new Student("Tom", "Johnson", "tom1@gmail.com", "IT2021");
+        Student anna = new Student("Anna", "Bell", "anna1@gmail.com", "IT2021");
+        Student layla = new Student("Layla", "Smith", "layla@gmail.com", "IT2021");
+        Student david = new Student("David", "Brown", "dev@gmail.com", "IT2021");
+
+        List<Student> students = new ArrayList<>();
+        Classroom it2021 = new Classroom(students);
+        it2021.addStudent(anna);
+        it2021.addStudent(tom);
+        it2021.addStudent(layla);
+        it2021.addStudent(david);
+
+        System.out.println("The all members of class:" + it2021.toString());
+
+        System.out.println("The count of students:" + it2021.studentCount());
+
+        it2021.deleteStudent(anna);
+        System.out.println("The count of students after deleting Anna:" + it2021.studentCount());
+
+
+
+
+
+    }
+}
