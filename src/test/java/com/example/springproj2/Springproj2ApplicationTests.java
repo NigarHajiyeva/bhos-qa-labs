c3ab22b716c60081349652f8a2321989145b5de1
NigarHajiyeva <64946039+NigarHajiyeva@users.noreply.github.com>
2021-10-29 23:26:14 +0400

    Validation SSL test added

--- src/test/java/com/example/springproj2/Springproj2ApplicationTests.java
+++ src/test/java/com/example/springproj2/Springproj2ApplicationTests.java
@@ -1,23 +1,34 @@
 package com.example.springproj2;
 
-
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.security.cert.*;
-import java.util.*;
-import java.util.*;
+import java.net.URLConnection;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Springproj2ApplicationTests {
@@ -25,27 +36,25 @@ public class Springproj2ApplicationTests {
     TestRestTemplate restTemplate = new TestRestTemplate();
     HttpHeaders headers = new HttpHeaders();
     public String url = "https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos";
-    
-    
+
     @Test
     @DisplayName("Integration Test to check SSL certificate")
-	void test1() throws CertificateException, IOException {
-		URL url = new URL(this.url);
+    void certValidation() throws CertificateException, IOException {
+        URL url = new URL(this.url);
 
-		URLConnection con = url.openConnection();
-		HttpsURLConnection securecon = (HttpsURLConnection) con;
-		securecon.connect();
+        URLConnection con = url.openConnection();
+        HttpsURLConnection securecon = (HttpsURLConnection) con;
+        securecon.connect();
 
-		Certificate[] certs = securecon.getServerCertificates();
+        Certificate[] certs = securecon.getServerCertificates();
 
-		FileInputStream fis = new FileInputStream("mockapicert.cer");
+        FileInputStream fis = new FileInputStream("cert.cer");
 
-		CertificateFactory fac = CertificateFactory.getInstance("X509");
-		X509Certificate cert = (X509Certificate) fac.generateCertificate(fis);
+        CertificateFactory fac = CertificateFactory.getInstance("X509");
+        X509Certificate cert = (X509Certificate) fac.generateCertificate(fis);
 
-		assertEquals(cert, certs[0]);
-	}
-
+        assertEquals(cert, certs[0]);
+    }
 
 
     @Test
@@ -62,10 +71,8 @@ public class Springproj2ApplicationTests {
             map.put(o.getString("id"), o.getString("name"));
 
         }
-        assertThat(map).containsKey("1");
+        assertTrue(map.containsKey(1) != map.containsKey(2));
 
     }
 
 }
-
-
