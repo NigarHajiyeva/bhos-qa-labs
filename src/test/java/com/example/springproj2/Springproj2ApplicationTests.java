9fbb377fdeff7e99634f96b66bdab27f9ecca99a
NigarHajiyeva <64946039+NigarHajiyeva@users.noreply.github.com>
2021-10-29 22:43:21 +0400

    add second IntegrationTest

--- src/test/java/com/example/springproj2/Springproj2ApplicationTests.java
+++ src/test/java/com/example/springproj2/Springproj2ApplicationTests.java
@@ -19,13 +19,35 @@ public class Springproj2ApplicationTests {
 
     TestRestTemplate restTemplate = new TestRestTemplate();
     HttpHeaders headers = new HttpHeaders();
+    public String url = "https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos";
+    
+    
+    @Test
+    @DisplayName("Integration Test to check SSL certificate")
+	void test1() throws CertificateException, IOException {
+		URL url = new URL(this.url);
+
+		URLConnection con = url.openConnection();
+		HttpsURLConnection securecon = (HttpsURLConnection) con;
+		securecon.connect();
+
+		Certificate[] certs = securecon.getServerCertificates();
+
+		FileInputStream fis = new FileInputStream("mockapicert.cer");
+
+		CertificateFactory fac = CertificateFactory.getInstance("X509");
+		X509Certificate cert = (X509Certificate) fac.generateCertificate(fis);
+
+		assertEquals(cert, certs[0]);
+	}
+
 
 
     @Test
     @DisplayName("Integration test of 'https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos' link")
     public void firstURLTest() throws Exception {
         HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<String> response = restTemplate.exchange("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos", HttpMethod.GET, entity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 
         JSONArray objects = new JSONArray(response.getBody());
         HashMap<String, String> map = new HashMap<>();
